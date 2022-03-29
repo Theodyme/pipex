a4e1ed01734be8e09849aea67a5789b4ce6de7af
Théophane P <theodyme@gmail.com>
2022-03-29 18:32:10 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -12,6 +12,50 @@
 
 #include "pipex.h"
 
+/*
+	WHAT WE GLOBALLY NEED TO EXEC AND CAN BE STRUCTURED IN THE EXECUTIVE FUNCTION:
+	char	*path;			is the path after going through the pathfinder.c.
+	char	**cmd;			is the array containing the command and arguments after going through the cmdbuilder.
+	char	**env;			the env variable.
+	int	*fds;			is the array containing the INFILE and OUTFILE fds after going through the fdsbuilder.
+
+	if you check that those parameters are valid before properly starting anything, you will barely have to worry about fd closing and variables that must be freed.
+
+	LEAKS HANDLING:
+	[x] free **env/paths= after building your 2nd command.
+	[x] free every path you try to build if they fail.
+	[x] be sure to close any unused fd before launching execve.
+	[x] close any open fd if anything fails and stops the process.
+	[x] you need to free the struct's elements at the potential end of the process if anything goes wrong.
+
+	WHICH VAR. WILL BE INSTANTIATED IN THE EXECUTIVE FUNCTION:
+	int	pid;			will contain the pid.
+	int	*fds[2];		is the array containing the pipes entry & exit.
+	int	status;			will contain the status of waitpid().
+
+	IF THE TIME LEFT ALLOWS IT, WHAT I MAY NEED IN THE STRUCTURE TO HANDLE ERROR CASES:
+	int	errno;
+
+	SPECIFIC CASE TO HANDLE:
+	[x]	any command | ls:		only executes ls.
+	[x]	echo "hello world" -n:		printsx "hello world" -n. (YOU DON'T HAVE TO HANDLE THIS CASE TO SUCCESS)
+	[x]	grep "o" -c:			prints the result and use the flag. (YOU DON'T HAVE TO HANDLE THIS CASE TO SUCCESS)
+						WHY ? First, because Pipex is not about parsing. As long as you understand how pipes, fork and fd manipulation work, you should be good. You can also argue that your commands are typed inside quotes, and that those cases will mess with how many arguments your program  will process.
+	[x]					if infile can't be open, you need to execute 2nd nonetheless.
+	[x]					if outfile can't be open, you need to execute 1st command nonetheless.
+	[x]					handle empty cmd.
+	[x]					you need to check if the **env parameter exists.
+	[x]					you need to check if you can actually find the line PATH= in the env parameter. This will ensure your program won't segfault if you unset PATH in the shell before running Pipex.
+	[x]					handle wrong first cmd.
+	[x]					handle wrong second cmd.
+
+	check leaks on linux with:
+	valgrind --leak-check=full --show-leak-kinds=all --trace-children=yes --tracks-fds=yes ./pipex -
+*/
+
+
+//	BUILDFREER: will free the struct's elements.
+
 void	buildfreer(t_bld *s)
 {
 	if (s->fdin != -1)
@@ -24,6 +68,8 @@ void	buildfreer(t_bld *s)
 	free(s->path[1]);
 	return ;
 }
+
+//	CHILDPROCESS: will handle the fds, pipe, and then execute the first command.
 
 int	childprocess(t_bld *s, int *pipe)
 {
@@ -44,6 +90,8 @@ int	childprocess(t_bld *s, int *pipe)
 	return (-1);
 }
 
+//	PARENTPROCESS: [should be renamed] will handle the fds, pipe, and then execute the second command.
+
 void	parentprocess(t_bld *s, int *pipe)
 {
 	if (!(s->path[1]))
@@ -61,6 +109,9 @@ void	parentprocess(t_bld *s, int *pipe)
 	execve(s->path[1], &(s->cmd[1][0]), s->env);
 	return ;
 }
+
+//	LAUNCHER: will fork your program to execute each command in their own children. will pipe before that to ensure you have an accessible pipe to use to pass information between process.
+//	waitpid() is executed at the end of the two forks to ensure the handling of commands like sleep, for which it is important that the two commands start at the same time.
 
 void	launcher(t_bld *s)
 {
@@ -90,6 +141,11 @@ void	launcher(t_bld *s)
 	}
 	return ;
 }
+
+//	MAIN: in which you will fill your structure with any necessary parameters to execute.
+//	You will need to get into pathbuilders functions to get the path string out of the **env parameter.
+//	I'm building the command and flags array to immediately check its validity.
+//	Only then do I launch pipex properly-speaking.
 
 int	main(int ac, char **av, char **env)
 {
